b59c9c7ae5592f4e64a2d398d9a0c4e1aa509be9
WadeeKT <thomassanna1e@gmail.com>
2024-09-05 10:32:19 +0200

    Exercice 5 Atelier 2: Agencement d'objets dans deux vitrines

---
+++ Atelier 2/Partie 2/Exercice 5/exo5.py
@@ -0,0 +1,76 @@
+# nombre d'emplacements disponibles par vitrine
+nbEmplacement = 4
+# liste des objets
+lObjets = [1, 2, 2, 3, 4, 5, 5]
+
+def maxOccurence(L):
+  """
+  Retourne le nombre maximum d'occurrences d'un élément dans la liste L.
+
+  Args:
+      L (list): La liste des éléments.
+
+  Returns:
+    int: Le nombre maximum d'occurrences d'un élément dans la liste.
+  """
+  maxi = 0
+  for i in range(len(L)):
+    # vérifie si l'élément courant a plus d'occurrences que le maximum actuel
+    if maxi < L.count(L[i]):
+      maxi = L.count(L[i])
+  return maxi
+
+def rangerParNombreOccurence(L):
+  """
+  Trie la liste L par nombre d'occurrences décroissantes des éléments.
+     
+  Args:
+      L (list): La liste des éléments.
+     
+  Returns:
+      list: La liste triée par nombre d'occurrences décroissantes.
+  """
+  L.sort()
+  maxOcc = maxOccurence(L)
+  # crée une liste de listes temporaires pour stocker les éléments par occurrences
+  lTemp = [[] for i in range(maxOcc)]
+  for elt in L:
+    # ajoute l'élément dans la liste temporaire correspondante
+    lTemp[L.count(elt)-1].append(elt)
+  lRes = []
+  for i in range(len(lTemp)):
+    # ajoute les éléments des listes temporaires à la liste de résultat
+    lRes += lTemp[len(lTemp)-(i+1)]
+  return lRes
+
+def agenceObjetVitrines(nbEmplacement, lObjets):
+  """
+  Organise les objets dans les vitrines en fonction du nombre d'emplacements disponibles.
+         
+  Args:
+      nbEmplacement (int): Le nombre d'emplacements disponibles par vitrine.
+      lObjets (list): La liste des objets à organiser.
+       
+  Returns:
+      list: Une liste de deux vitrines contenant les objets organisés.
+  """
+  lParOccurence = rangerParNombreOccurence(lObjets)
+  vitrines = ([], [])
+  for i in range(len(vitrines)):
+    nbEmpPris = 0
+    j = 0
+    while j < len(lParOccurence) and nbEmpPris < nbEmplacement:
+      # vérifie si l'objet n'est pas déjà dans la vitrine courante
+      if lParOccurence[j] not in vitrines[i]:
+        vitrines[i].append(lParOccurence[j])
+        lParOccurence.pop(j)
+        nbEmpPris += 1
+      else:
+        j += 1
+  # trie les objets par ordre croissant dans chaque vitrine
+  vitrines = map(sorted, vitrines)
+  # Renvoie les deux vitrines et renvoie si les objets ont bien tous été pris ou non.
+  return (list(vitrines), len(lParOccurence) == 0) 
+
+# affiche le résultat de l'organisation des objets dans les vitrines
+print(agenceObjetVitrines(nbEmplacement, lObjets))
