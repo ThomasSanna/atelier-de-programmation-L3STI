a9dbd0fe8d4de5226d6c14e3437742738d93d68b
WadeeKT <thomassanna1e@gmail.com>
2024-09-06 10:16:43 +0200

    Ajout Docstring

--- Atelier 3/Exercice 2/main.py
+++ Atelier 3/Exercice 2/main.py
@@ -1,44 +1,112 @@
-lstMot=["jouer","bonjour", "punir", "jour", "aurevoir", "revoir", "pouvoir", "cour", "abajour", 
+lstMot = ["jouer", "bonjour", "punir", "jour", "aurevoir", "revoir", "pouvoir", "cour", "abajour", 
 "finir", "aimer"]
 
-def motsNLettres(lstMot:list, n:int)->list:
-  lstRes = []
-  for mot in lstMot:
-    if len(mot) == n:
-      lstRes.append(mot)
-  return lstRes
+def motsNLettres(lstMot: list, n: int) -> list:
+    """
+    Retourne une liste de mots ayant exactement n lettres.
 
-def commencePar(mot:str, prefixe:str)->bool:
-  longPrefixe = len(prefixe)
-  return  mot[:longPrefixe] == prefixe # ou "prefixe in mot" mais bon
-  # Ex: "Salut"[0] == "S", "Salut"[2] == 'l', "Salut"[:2] == "Sal"
+    Args:
+        lstMot (list): La liste des mots à filtrer.
+        n (int): Le nombre de lettres que les mots doivent avoir.
 
+    Returns:
+        list: Une liste de mots ayant exactement n lettres.
+    """
+    lstRes = []
+    for mot in lstMot:
+        if len(mot) == n:
+            lstRes.append(mot)
+    return lstRes
+
+def commencePar(mot: str, prefixe: str) -> bool:
+    """
+    Vérifie si un mot commence par un préfixe donné.
+
+    Args:
+        mot (str): Le mot à vérifier.
+        prefixe (str): Le préfixe à chercher au début du mot.
+
+    Returns:
+        bool: True si le mot commence par le préfixe, False sinon.
+    """
+    longPrefixe = len(prefixe)
+    return mot[:longPrefixe] == prefixe
 
 assert commencePar('along', 'alo') == True
 
-def finitPar(mot:str, suffixe:str)->bool:
-  longSuffixe = len(suffixe)
-  return mot[-longSuffixe:] == suffixe # ou "suffixe in mot" mais bon
-  # Ex: "Salut"[-1] == "t", "Salut"[-2] == 'u', "Salut"[-2:] == "ut"
+def finitPar(mot: str, suffixe: str) -> bool:
+    """
+    Vérifie si un mot finit par un suffixe donné.
+
+    Args:
+        mot (str): Le mot à vérifier.
+        suffixe (str): Le suffixe à chercher à la fin du mot.
+
+    Returns:
+        bool: True si le mot finit par le suffixe, False sinon.
+    """
+    longSuffixe = len(suffixe)
+    return mot[-longSuffixe:] == suffixe
 
 assert finitPar("along", "long") == True
 
-def finissentPar(lstMot:list, suffixe:str)->list:
-  return [mot for mot in lstMot if finitPar(mot, suffixe)]
+def finissentPar(lstMot: list, suffixe: str) -> list:
+    """
+    Retourne une liste de mots finissant par un suffixe donné.
+
+    Args:
+        lstMot (list): La liste des mots à filtrer.
+        suffixe (str): Le suffixe que les mots doivent avoir à la fin.
+
+    Returns:
+        list: Une liste de mots finissant par le suffixe.
+    """
+    return [mot for mot in lstMot if finitPar(mot, suffixe)]
 
 assert finissentPar(lstMot, 'jour') == ['bonjour', 'jour', 'abajour']
 
-def commencentPar(lstMot:list, prefixe:str)->list:
-  return [mot for mot in lstMot if commencePar(mot, prefixe)]
+def commencentPar(lstMot: list, prefixe: str) -> list:
+    """
+    Retourne une liste de mots commençant par un préfixe donné.
 
-def listeMots(lstMot:list, prefixe:str, suffixe:str, n:int)->str:
-  return [mot for mot in lstMot if commencePar(mot, prefixe) and finitPar(mot, suffixe) and motsNLettres(lstMot, n)]
+    Args:
+        lstMot (list): La liste des mots à filtrer.
+        prefixe (str): Le préfixe que les mots doivent avoir au début.
 
-def dictionnaire(fichier:str)->list:
-  lstRes = []
-  f = open(fichier, 'r', encoding='utf-8')
-  for line in f:
-    lstRes.append(line[:-1]) # "[:-1]" enleve les "/n"
-  return lstRes
+    Returns:
+        list: Une liste de mots commençant par le préfixe.
+    """
+    return [mot for mot in lstMot if commencePar(mot, prefixe)]
+
+def listeMots(lstMot: list, prefixe: str, suffixe: str, n: int) -> list:
+    """
+    Retourne une liste de mots qui commencent par un préfixe, finissent par un suffixe et ont une longueur donnée.
+
+    Args:
+        lstMot (list): La liste des mots à filtrer.
+        prefixe (str): Le préfixe que les mots doivent avoir au début.
+        suffixe (str): Le suffixe que les mots doivent avoir à la fin.
+        n (int): La longueur que les mots doivent avoir.
+
+    Returns:
+        list: Une liste de mots répondant aux critères.
+    """
+    return [mot for mot in lstMot if commencePar(mot, prefixe) and finitPar(mot, suffixe) and len(mot) == n]
+
+def dictionnaire(fichier: str) -> list:
+    """
+    Lit un fichier et retourne une liste de mots.
+
+    Args:
+        fichier (str): Le chemin du fichier à lire.
+
+    Returns:
+        list: Une liste de mots contenus dans le fichier.
+    """
+    lstRes = []
+    with open(fichier, 'r', encoding='utf-8') as f:
+        for line in f:
+            lstRes.append(line.strip())  # Utilisation de strip() pour enlever les espaces et les sauts de ligne
+    return lstRes
 
 assert dictionnaire('Atelier 3/Exercice 2/mots.txt') == ['bonjour', 'bonsoir', 'ça', 'va', 'moi', 'super', 'merci', 'je', 'vous', 'en', 'prie', 'non', 'pas', 'de', 'soucis']
---
+++ Atelier 3/Exercice 3/main.py
@@ -0,0 +1,92 @@
+import random
+
+def placesLettre(ch: str, mot: str) -> list:
+    """
+    Retourne les positions d'une lettre dans un mot.
+
+    Args:
+        ch (str): La lettre à chercher dans le mot.
+        mot (str): Le mot dans lequel chercher la lettre.
+
+    Returns:
+        list: Une liste des positions de la lettre dans le mot.
+
+    Raises:
+        ValueError: Si l'entrée de la lettre n'est pas valide (longueur différente de 1).
+    """
+    indRes = []
+    if len(ch) != 1:
+        raise ValueError("Un problème est survenu lors de l'entrée de la lettre")
+    for i, elt in enumerate(mot):
+        if elt == ch:
+            indRes.append(i)
+    return indRes
+
+assert placesLettre("m", 'maman') == [0, 2]
+
+def outputStr(mot: str, lPos: list) -> str:
+    """
+    Retourne une chaîne de caractères avec des lettres trouvées et des tirets pour les lettres non trouvées.
+
+    Args:
+        mot (str): Le mot à afficher partiellement.
+        lPos (list): La liste des positions des lettres trouvées.
+
+    Returns:
+        str: Une chaîne de caractères avec les lettres trouvées et des tirets pour les lettres non trouvées.
+    """
+    res = ''
+    for i in range(len(mot)):
+        if i in lPos:
+            res += mot[i]
+        else:
+            res += '-'
+    return res
+
+assert outputStr('bonjour', [0, 1, 4]) == 'bo--o--'
+
+lstPendu = ["|---] ", 
+            "| O ", 
+            "| T ", 
+            "|/ \ ", 
+            "|______"]
+
+def runGame():
+    """
+    Lance le jeu du pendu. Le joueur doit deviner un mot en entrant des lettres une par une.
+
+    Le jeu continue jusqu'à ce que le joueur trouve toutes les lettres du mot ou fasse 5 erreurs.
+    """
+    lstMot = [
+        "jouer", "bonjour", "punir", "jour", "aurevoir", "revoir", "pouvoir", "cour", "abajour", "finir", "aimer"
+    ]
+    indRandom = random.randint(0, len(lstMot) - 1)
+    motATrouver = lstMot[indRandom]
+    nbErreur = 0
+    lstIndTrouve = []
+
+    while nbErreur < 5 and len(lstIndTrouve) != len(motATrouver):
+        print("\nMot à trouver:")
+        print(outputStr(motATrouver, lstIndTrouve))
+        
+        try:
+            chr = str(input('\nEntrez une lettre (minuscule) -> '))
+            chr = chr.lower()
+        except:
+            raise ValueError("\nUn problème est survenu lors de l'entrée de la lettre")
+          
+        lstIndRes = placesLettre(chr, motATrouver)
+        if lstIndRes == []:
+            print("\nErreur! La lettre " + chr + " n'est pas dans le mot cherché ! \n")
+            nbErreur += 1
+            for i in range(nbErreur):
+                print(lstPendu[i])
+        else:
+            lstIndTrouve += lstIndRes
+
+    if nbErreur == 5:
+        print("\n Perdu !")
+    else:
+        print("Bravo ! Le mot était bien ", motATrouver, "!")
+
+runGame()
