a0a0f3452bc252b7317dab8150d6fa0c2913cce5
WadeeKT <thomassanna1e@gmail.com>
2024-09-09 15:14:51 +0200

    Atelier 3 Exo 2

--- Atelier 3/Exercice 2/main.py
+++ Atelier 3/Exercice 2/main.py
@@ -1,6 +1,3 @@
-lstMot = ["jouer", "bonjour", "punir", "jour", "aurevoir", "revoir", "pouvoir", "cour", "abajour", 
-"finir", "aimer"]
-
 def motsNLettres(lstMot: list, n: int) -> list:
     """
     Retourne une liste de mots ayant exactement n lettres.
@@ -18,6 +15,7 @@ def motsNLettres(lstMot: list, n: int) -> list:
             lstRes.append(mot)
     return lstRes
 
+
 def commencePar(mot: str, prefixe: str) -> bool:
     """
     Vérifie si un mot commence par un préfixe donné.
@@ -29,10 +27,12 @@ def commencePar(mot: str, prefixe: str) -> bool:
     Returns:
         bool: True si le mot commence par le préfixe, False sinon.
     """
+    if len(mot) < len(prefixe):
+        raise ValueError('Le préfixe est plus grand que le mot, en gros')
+    
     longPrefixe = len(prefixe)
     return mot[:longPrefixe] == prefixe
 
-assert commencePar('along', 'alo') == True
 
 def finitPar(mot: str, suffixe: str) -> bool:
     """
@@ -45,10 +45,12 @@ def finitPar(mot: str, suffixe: str) -> bool:
     Returns:
         bool: True si le mot finit par le suffixe, False sinon.
     """
+    if len(mot) < len(suffixe):
+        raise ValueError('Le suffixe est plus grand que le mot, en gros')
+    
     longSuffixe = len(suffixe)
     return mot[-longSuffixe:] == suffixe
 
-assert finitPar("along", "long") == True
 
 def finissentPar(lstMot: list, suffixe: str) -> list:
     """
@@ -63,7 +65,6 @@ def finissentPar(lstMot: list, suffixe: str) -> list:
     """
     return [mot for mot in lstMot if finitPar(mot, suffixe)]
 
-assert finissentPar(lstMot, 'jour') == ['bonjour', 'jour', 'abajour']
 
 def commencentPar(lstMot: list, prefixe: str) -> list:
     """
@@ -78,20 +79,25 @@ def commencentPar(lstMot: list, prefixe: str) -> list:
     """
     return [mot for mot in lstMot if commencePar(mot, prefixe)]
 
-def listeMots(lstMot: list, prefixe: str, suffixe: str, n: int) -> list:
+
+def listeMots(lstMots: list, prefixe: str, suffixe: str, longueur: int) -> list:
     """
     Retourne une liste de mots qui commencent par un préfixe, finissent par un suffixe et ont une longueur donnée.
 
     Args:
-        lstMot (list): La liste des mots à filtrer.
+        lstMots (list): La liste des mots à filtrer.
         prefixe (str): Le préfixe que les mots doivent avoir au début.
         suffixe (str): Le suffixe que les mots doivent avoir à la fin.
-        n (int): La longueur que les mots doivent avoir.
+        longueur (int): La longueur que les mots doivent avoir.
 
     Returns:
         list: Une liste de mots répondant aux critères.
     """
-    return [mot for mot in lstMot if commencePar(mot, prefixe) and finitPar(mot, suffixe) and len(mot) == n]
+    motsPrefixe = commencentPar(lstMots, prefixe)
+    motsSuffixe = finissentPar(motsPrefixe, suffixe)
+    motsLongueurDonnee = motsNLettres(motsSuffixe, longueur)
+    return motsLongueurDonnee
+    # return list(set(commencentPar(lstMot, prefixe)) & set(finissentPar(lstMot, suffixe)) & set(motsNLettres(lstMot, n)))
 
 def dictionnaire(fichier: str) -> list:
     """
@@ -108,5 +114,34 @@ def dictionnaire(fichier: str) -> list:
         for line in f:
             lstRes.append(line.strip())  # Utilisation de strip() pour enlever les espaces et les sauts de ligne
     return lstRes
+
+
+# Liste de mots pour les tests
+LST_MOT = ["jouer", "bonjour", "punir", "jour", "aurevoir", "revoir", "pouvoir", "cour", "abajour", 
+"finir", "aimer"]
+
+assert motsNLettres(LST_MOT, 5) == ['jouer', 'punir', 'finir', 'aimer']
+assert motsNLettres(LST_MOT, 4) == ["jour", "cour"]
+assert motsNLettres(LST_MOT, 7) == ['bonjour', 'pouvoir', 'abajour']
+
+assert commencePar("bonjour", "bon") == True
+assert commencePar("bonjour", "jour") == False
+assert commencePar("aimer", "aim") == True
+
+assert finitPar("bonjour", "jour") == True
+assert finitPar("bonjour", "bon") == False
+assert finitPar("aimer", "mer") == True
+
+assert finissentPar(LST_MOT, "jour") == ["bonjour", "jour", "abajour"]
+assert finissentPar(LST_MOT, "voir") == ["aurevoir", "revoir", "pouvoir"]
+assert finissentPar(LST_MOT, "ir") == ['punir', 'aurevoir', 'revoir', 'pouvoir', 'finir']
+
+assert commencentPar(LST_MOT, "bon") == ["bonjour"]
+assert commencentPar(LST_MOT, "re") == ["revoir"]
+assert commencentPar(LST_MOT, "a") == ["aurevoir", "abajour", "aimer"]
+
+assert listeMots(LST_MOT, "bon", "jour", 7) == ["bonjour"]
+assert listeMots(LST_MOT, "a", "ir", 5) == []
+assert listeMots(LST_MOT, "re", "ir", 6) == ["revoir"]
 
 assert dictionnaire('Atelier 3/Exercice 2/mots.txt') == ['bonjour', 'bonsoir', 'ça', 'va', 'moi', 'super', 'merci', 'je', 'vous', 'en', 'prie', 'non', 'pas', 'de', 'soucis']
