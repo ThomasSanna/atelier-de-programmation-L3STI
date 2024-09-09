844050346bc57a407b33b95321f4e7254b7b166a
WadeeKT <thomassanna1e@gmail.com>
2024-09-09 14:07:36 +0200

    refactoring LSEP

--- Atelier 2/Partie 1/Exercice 3/main.py
+++ Atelier 2/Partie 1/Exercice 3/main.py
@@ -1,15 +1,15 @@
 def separer(L:list)->list:
   indNeg = 0
   indPos = -1
-  LSEP = [0 for i in range(len(L))]
+  lsep = [0 for i in range(len(L))]
   for elt in L:
     if elt < 0:
-      LSEP[indNeg] = elt
+      lsep[indNeg] = elt
       indNeg += 1
     elif elt > 0:
-      LSEP[indPos] = elt
+      lsep[indPos] = elt
       indPos -= 1
-  return LSEP
+  return lsep
 
 L = [0, -2, 1, 6, -4, -3, 2, 8, -2, 5, 6, 0, 0, 8, -38]
 print(separer(L))
