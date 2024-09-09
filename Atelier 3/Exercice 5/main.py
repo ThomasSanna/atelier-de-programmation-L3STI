b4cb32409b4f6623449b6666cd82aa6c96f551c5
WadeeKT <thomassanna1e@gmail.com>
2024-09-09 14:02:56 +0200

    Exercice 5 Atelier 3

---
+++ Atelier 3/Exercice 5/main.py
@@ -0,0 +1,62 @@
+def ouvrante(car):
+    """Renvoie True si le caractère est une parenthèse, un crochet ou une accolade ouvrante."""
+    return car in '({['
+
+def fermante(car):
+    """Renvoie True si le caractère est une parenthèse, un crochet ou une accolade fermante."""
+    return car in ')}]'
+
+def renverse(car):
+    """Renvoie le caractère correspondant à la parenthèse, au crochet ou à l'accolade fermante."""
+    correspondance = {')': '(', '}': '{', ']': '['}
+    return correspondance.get(car, car)  # si 'car' n'est pas dans le dictionnaire, on renvoie car sans le modifier
+
+def operateur(car):
+    """Renvoie True si le caractère est un opérateur (+, * ou -)."""
+    return car in '+*-'
+
+def nombre(car):
+    """Renvoie True si la chaîne de caractères ne comporte que des caractères numériques."""
+    return car.isdigit()
+
+def caractereValide(car):
+    """Renvoie True si le caractère est valide dans une expression arithmétique."""
+    return car in '(){}[]+-* ' or car.isdigit()
+
+assert ouvrante('(') == True
+assert ouvrante(')') == False
+assert fermante(')') == True
+assert fermante('(') == False
+assert renverse(')') == '('
+assert renverse('(') == '('
+assert operateur('+') == True
+assert operateur('/') == False
+assert nombre('123') == True
+assert nombre('abc') == False
+assert caractereValide('1') == True
+assert caractereValide('a') == False
+
+
+def verif_parenthese(expression: str) -> bool:
+    P = []
+    correspondances = {')': '(', '}': '{', ']': '['}
+    
+    for char in expression:
+        if ouvrante(char):
+            # empiler les parenthèses ouvrantes
+            P.append(char)
+        elif fermante(char):
+            # vérifier si la pile est vide ou si le sommet de la pile ne correspond pas
+            if P==[] or P[-1] != correspondances[char]:
+                return False
+            # dépiler le sommet de la pile
+            P.pop()
+        elif not caractereValide(char):
+            return False
+    
+    # vérifier si la pile est vide
+    return not P
+
+print(verif_parenthese("(3+2) * 6-1"))  # True
+print(verif_parenthese("((3+2)*6-1"))   # False
+print(verif_parenthese("(5+7]*12"))     # False
